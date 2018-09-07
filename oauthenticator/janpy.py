28d39179a9a0db38ba6485f558278bfb8c6864fd
janpychou <janpychou@qq.com>
2018-09-07 18:35:28 +0800

    add custom OAuthenticator for oltu-oauth2-example
    #oltu-oauth2-example

---
+++ oauthenticator/janpy.py
@@ -0,0 +1,167 @@
+"""
+Custom Authenticator to use generic OAuth2 with JupyterHub
+"""
+
+
+import json
+import os
+import base64
+import urllib
+
+from tornado.auth import OAuth2Mixin
+from tornado import gen, web
+
+from tornado.httputil import url_concat
+from tornado.httpclient import HTTPRequest, AsyncHTTPClient
+
+from jupyterhub.auth import LocalAuthenticator
+
+from traitlets import Unicode, Dict, Bool
+
+from .oauth2 import OAuthLoginHandler, OAuthenticator
+
+
+class JanpyEnvMixin(OAuth2Mixin):
+    _OAUTH_ACCESS_TOKEN_URL = os.environ.get('OAUTH2_TOKEN_URL', '')
+    _OAUTH_AUTHORIZE_URL = os.environ.get('OAUTH2_AUTHORIZE_URL', '')
+
+
+class JanpyLoginHandler(OAuthLoginHandler, JanpyEnvMixin):
+    pass
+
+
+class JanpyOAuthenticator(OAuthenticator):
+
+    login_service = Unicode(
+        "GenericOAuth2",
+        config=True
+    )
+
+    login_handler = JanpyLoginHandler
+
+    userdata_url = Unicode(
+        os.environ.get('OAUTH2_USERDATA_URL', ''),
+        config=True,
+        help="Userdata url to get user data login information"
+    )
+    token_url = Unicode(
+        os.environ.get('OAUTH2_TOKEN_URL', ''),
+        config=True,
+        help="Access token endpoint URL"
+    )
+    extra_params = Dict(
+        os.environ.get('OAUTH2_AUTHENTICATION_PARAMS', {}),
+        help="Extra parameters for first POST request"
+    ).tag(config=True)
+
+    username_key = Unicode(
+        os.environ.get('OAUTH2_USERNAME_KEY', 'username'),
+        config=True,
+        help="Userdata username key from returned json for USERDATA_URL"
+    )
+    userdata_params = Dict(
+        os.environ.get('OAUTH2_USERDATA_PARAMS', {}),
+        help="Userdata params to get user data login information"
+    ).tag(config=True)
+
+    userdata_method = Unicode(
+        os.environ.get('OAUTH2_USERDATA_METHOD', 'GET'),
+        config=True,
+        help="Userdata method to get user data login information"
+    )
+
+    tls_verify = Bool(
+        os.environ.get('OAUTH2_TLS_VERIFY', 'True').lower() in {'true', '1'},
+        config=True,
+        help="Disable TLS verification on http request"
+    )
+
+    @gen.coroutine
+    def authenticate(self, handler, data=None):
+        code = handler.get_argument("code")
+        print(code)
+        # TODO: Configure the curl_httpclient for tornado
+        http_client = AsyncHTTPClient()
+
+        params = dict(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            redirect_uri=self.get_callback_url(handler),
+            code=code,
+            grant_type='authorization_code'
+        )
+        print(params)
+        params.update(self.extra_params)
+
+        if self.token_url:
+            url = self.token_url
+        else:
+            raise ValueError("Please set the OAUTH2_TOKEN_URL environment variable")
+
+        b64key = base64.b64encode(
+            bytes(
+                "{}:{}".format(self.client_id, self.client_secret),
+                "utf8"
+            )
+        )
+
+        headers = {
+            "Accept": "application/json",
+            "User-Agent": "JupyterHub",
+            "Authorization": "Basic {}".format(b64key.decode("utf8"))
+        }
+        req = HTTPRequest(url,
+                          method="POST",
+                          headers=headers,
+                          validate_cert=self.tls_verify,
+                          body=urllib.parse.urlencode(params)  # Body is required for a POST...
+                          )
+
+        resp = yield http_client.fetch(req)
+
+        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+        print(resp_json)
+
+        access_token = resp_json['access_token']
+        # refresh_token = resp_json.get('refresh_token', None)
+        # token_type = resp_json['token_type']
+        # scope = (resp_json.get('scope', '')).split(' ')
+
+        # Determine who the logged in user is
+        # headers = {
+        #     "Accept": "application/json",
+        #     "User-Agent": "JupyterHub",
+        #     "Authorization": "{} {}".format(token_type, access_token)
+        # }
+        if self.userdata_url:
+            url = "{}?access_token={}".format(self.userdata_url, access_token)
+            # url = url_concat(self.userdata_url, self.userdata_params)
+        else:
+            raise ValueError("Please set the OAUTH2_USERDATA_URL environment variable")
+
+        req = HTTPRequest(url,
+                          method="GET"
+                          )
+        resp = yield http_client.fetch(req)
+        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+        print(resp_json)
+        if not resp_json.get(self.username_key):
+            self.log.error("OAuth user contains no key %s: %s", self.username_key, resp_json)
+            return
+
+        return resp_json.get(self.username_key);
+        # return {
+        #     'name': resp_json.get(self.username_key),
+        #     'auth_state': {
+        #         'access_token': access_token,
+        #         'refresh_token': access_token,
+        #         'oauth_user': resp_json,
+        #         'scope': "user",
+        #     }
+        # }
+
+
+class LocalGenericOAuthenticator(LocalAuthenticator, JanpyOAuthenticator):
+
+    """A version that mixes in local system user creation"""
+    pass
